554e86a6e2557a7348dd6d95fd8dcb59f673af7e
Ben <ben@rpowerlabs.org>
2019-05-28 08:16:29 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -o errexit -o nounset
+BASE_REPO=$PWD
+
+setup_git() {
+Rscript -e 'source("R/rladies.R")'
+
+  git config --global user.name "benubah"
+  git config --global user.email "ben@rpowerlabs.org"
+}
+
+commit_files() {
+  git checkout master
+  # Current month and year, e.g: May 2019
+  dateAndMonth=`date "+%b %Y"`
+  # Stage the modified files in dist/output
+  git add -f data/rladies.csv
+  # Create a new commit with a custom build message
+  # with "[skip ci]" to avoid a build loop
+  # and Travis build number for reference
+  git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+}
+
+upload_files() {
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
+  git push origin master --quiet
+}
+
+setup_git
+
+commit_files
+
+upload_files
