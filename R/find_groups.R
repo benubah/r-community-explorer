c08760a08cbe1c778b91b799680c82d7d5855fff
Ben <ben@rpowerlabs.org>
2019-05-28 08:02:24 +0100

    Create find_groups.R

---
+++ R/find_groups.R
@@ -0,0 +1,35 @@
+find_groups <- function(text = NULL, topic_id = NULL, radius = "global", fields = NULL, api_key = NULL) {
+  api_method <- "find/groups"
+  res <- .fetch_results(api_method = api_method,
+                        api_key = api_key,
+                        text = text,
+                        topic_id = topic_id,
+                        fields = fields,
+                        radius = radius)
+  tibble::tibble(
+    id = purrr::map_int(res, "id"),
+    name = purrr::map_chr(res, "name"),
+    urlname = purrr::map_chr(res, "urlname"),
+    link = purrr::map_chr(res, "link"),
+    created = .date_helper(purrr::map_dbl(res, "created")),
+    members = purrr::map_int(res, "members"),
+    status = purrr::map_chr(res, "status"),
+    organizer = purrr::map_chr(res, c("organizer", "name")),
+    lat = purrr::map_dbl(res, "lat"),
+    lon = purrr::map_dbl(res, "lon"),
+    city = purrr::map_chr(res, "city"),
+    state = purrr::map_chr(res, "state", .null = NA),
+    country = purrr::map_chr(res, "localized_country_name"),
+    timezone = purrr::map_chr(res, "timezone", .null = NA),
+    join_mode = purrr::map_chr(res, "join_mode", .null = NA),
+    visibility = purrr::map_chr(res, "visibility", .null = NA),
+    who = purrr::map_chr(res, "who", .null = NA),
+    organizer_id = purrr::map_int(res, c("organizer", "id")),
+    organizer_name = purrr::map_chr(res, c("organizer", "name")),
+    category_id = purrr::map_int(res, c("category", "id"), .null = NA),
+    category_name = purrr::map_chr(res, c("category", "name"), .null = NA),
+    past_events = purrr::map_dbl(res, "past_event_count", .default = 0),
+    upcoming_events = purrr::map_dbl(res, "upcoming_event_count", .default = 0),
+    resource = res
+  )
+}
