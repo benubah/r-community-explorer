07ee1645e22c6b91b6a27b20a4914bf91dd23444
Ben <ben@rpowerlabs.org>
2019-05-30 22:17:14 +0100

    Update rladies.R

--- R/rladies.R
+++ R/rladies.R
@@ -1,11 +1,11 @@
 #library(meetupr)
-
+library(rcommunityexplorer)
 get_rladies <- function() {
  
 meetup_api_key <- Sys.getenv("MEETUP_KEY")
  
 # retrieve both past and upcoming event counts while finding groups
-  all_rladies_groups <- rcommunityexplorer::find_groups(text = "r-ladies", fields = "past_event_count, upcoming_event_count", api_key = meetup_api_key)
+  all_rladies_groups <- find_groups(text = "r-ladies", fields = "past_event_count, upcoming_event_count", api_key = meetup_api_key)
 
 # Cleanup
 rladies_groups <- all_rladies_groups[grep(pattern = "rladies|r-ladies|r ladies",  x = all_rladies_groups$name, ignore.case = TRUE), ]  
