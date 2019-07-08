c617267b890d93029be44d92a96101ffab41ad7e
Ben <ben@rpowerlabs.org>
2019-07-08 17:17:10 +0100

    git add rugs_map and rugs_cumulative files for commit

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,8 @@ commit_files() {
   git add -f docs/data/rladies_cumulative.json
   git add -f docs/data/rladies_map_data.geojson
   git add -f docs/data/rugs.csv
+  git add -f docs/data/rugs_cumulative.json
+  git add -f docs/data/rugs_map_data.geojson
   # Create a new commit with a custom build message
   # with "[skip ci]" to avoid a build loop
   # and Travis build number for reference
